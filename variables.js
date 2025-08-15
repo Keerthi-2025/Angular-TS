476c3e386c5fb75cd8d7ffcfc280e33371590f8a
Keerthi <rhkeerthi3@gmail.com>
2025-08-15 19:16:24 +0530

    first commit

---
+++ variables.js
@@ -0,0 +1,17 @@
+var x1 = 10;
+console.log(x1);
+var x2 = 10.3;
+console.log(x2);
+var x3 = "mike";
+console.log(x3);
+var x4 = true;
+console.log(x4);
+var x5 = 100; //can give anything
+var obj = {
+    "name": "mike",
+    "age": 12 //json ojbect
+};
+console.log(obj.name);
+console.log(obj.age);
+var arr = [10, 20, 30, 40, 50];
+console.log[arr[4]];
---
+++ variables.ts
@@ -0,0 +1,25 @@
+var x1:number = 10;
+console.log(x1);
+
+var x2:number=10.3;
+console.log(x2);
+
+var x3:string = "mike";
+console.log(x3);
+
+var x4:boolean =true;
+console.log(x4);
+
+var x5:any = 100;    //can give anything
+
+var obj={
+    "name":"mike",
+    "age":12                //json ojbect
+}
+console.log(obj.name);
+console.log(obj.age);
+
+
+
+var arr:number[] = [10, 20, 30, 40, 50];
+console.log[arr[4]];
